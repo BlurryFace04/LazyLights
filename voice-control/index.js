8fd2a09bb389f8c0241052cce5a5a9c52ec11166
Arnav Jain <64888928+BlurryFace04@users.noreply.github.com>
2023-08-02 15:05:34 +0530

    Update index.js

--- voice-control/index.js
+++ voice-control/index.js
@@ -3,14 +3,16 @@ const {
   SinricPro, startSinricPro
 } = require('sinricpro');
 
-const appKey = '1433ab95-e75a-42fd-992b-65004223fef7';
-const device1 = '64c9220635be2dbc3edd46d7';
-const device2 = '64c9225335be2dbc3edd4765';
-const secretKey = '69932de1-e937-4d7b-9c48-dfe025427d50-94f3e47e-62ac-4cf6-887e-1d3380b956b6';
-const deviceIds = [device1, device2];
+const appKey = 'API-KEY';
+const device1 = 'DEVICE1-ID';
+const device2 = 'DEVICE2-ID';
+const device3 = 'DEVICE3-ID';
+const device4 = 'DEVICE4-ID';
+const secretKey = 'SECRET-KEY';
+const deviceIds = [device1, device2, device3, device4];
 
 const send_command_to_device = async (appliance_name) => {
-  const url = 'http://192.168.91.53:5069/jsonex';
+  const url = 'http://192.168.1.3:5069/jsonex';
   const data = {
     appliance: appliance_name
   };
@@ -22,6 +24,10 @@ const setPowerState = async (deviceId, data) => {
     await send_command_to_device('tl');
   } else if (deviceId === device2) {
     await send_command_to_device('bl');
+  } else if (deviceId === device3) {
+    await send_command_to_device('f');
+  } else if (deviceId === device4) {
+    await send_command_to_device('s');
   }
   return true;
 };
